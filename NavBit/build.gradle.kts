d238cd6f5794dcecd0532ce68fcf36dcf5a16407
Gustav Nilsson <GustavN@users.noreply.github.com>
2025-02-25 15:09:48 +0100

    3.4.2 - Update dependencies and target api

--- NavBit/build.gradle.kts
+++ NavBit/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 
 android {
     namespace = "se.quidbit.navbit"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         minSdk = 21
@@ -43,7 +43,7 @@ android {
 }
 
 fun versionName(): String {
-    return "3.4.1"
+    return "3.4.2"
 }
 
 val githubProperties = Properties().apply {
@@ -73,18 +73,18 @@ publishing {
 }
 
 dependencies {
-    implementation("androidx.compose.animation:animation-android:1.7.4")
-    implementation("androidx.compose.material3:material3-android:1.3.0")
+    implementation("androidx.compose.animation:animation-android:1.7.8")
+    implementation("androidx.compose.material3:material3-android:1.3.1")
     val composeBom = platform("androidx.compose:compose-bom:2024.09.00")
-    val lifecycleVersion = "2.8.6"
+    val lifecycleVersion = "2.8.7"
 
     implementation(composeBom)
     androidTestImplementation(composeBom)
 
     implementation("androidx.compose.compiler:compiler:1.5.15")
 
-    implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.activity:activity-compose:1.9.3")
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.activity:activity-compose:1.10.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycleVersion")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleVersion")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:$lifecycleVersion")
--- demoApp/build.gradle.kts
+++ demoApp/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "se.soderstrom.navbitdemo"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "se.soderstrom.navbitdemo"
-        minSdk = 23
-        targetSdk = 34
+        minSdk = 21
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -43,16 +43,16 @@ android {
 dependencies {
     implementation("androidx.compose.compiler:compiler:1.5.15")
 
-    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation(project(mapOf("path" to ":NavBit")))
-    implementation("androidx.compose.foundation:foundation-layout-android:1.7.0")
-    implementation("androidx.compose.material3:material3-android:1.3.0")
-    implementation("androidx.compose.ui:ui-tooling-preview-android:1.7.0")
+    implementation("androidx.compose.foundation:foundation-layout-android:1.7.8")
+    implementation("androidx.compose.material3:material3-android:1.3.1")
+    implementation("androidx.compose.ui:ui-tooling-preview-android:1.7.8")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 
-    debugImplementation("androidx.compose.ui:ui-tooling:1.7.0")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.7.8")
 }
